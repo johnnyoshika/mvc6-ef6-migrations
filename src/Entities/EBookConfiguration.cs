6917f188b261d0c7383b74dbec15b63f129baa6f
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-11 22:20:05 -0800

    Add Entities

---
+++ src/Entities/EBookConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class EBookConfiguration : EntityTypeConfiguration<EBook>
+    {
+        public EBookConfiguration()
+        {
+            ToTable("Books");
+
+            Property(b => b.Title)
+                .HasMaxLength(200)
+                .IsRequired();
+        }
+    }
+}
---
+++ src/Entities/StoreContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    //[DbConfigurationType(typeof(CodeConfig))]
+    public class StoreContext : DbContext
+    {
+
+        static StoreContext()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<StoreContext>());
+        }
+
+        public StoreContext()
+            : base("Server=.;Database=MVC6_EF6_Migrations;Integrated Security=SSPI;")
+        {
+        }
+
+        public DbSet<EBook> Books { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Configurations.Add(new EBookConfiguration());
+        }
+    }
+
+    //class CodeConfig : DbConfiguration
+    //{
+    //    public CodeConfig()
+    //    {
+    //        SetDefaultConnectionFactory(new System.Data.Entity.Infrastructure.SqlConnectionFactory());
+    //        SetProviderServices("System.Data.SqlClient",
+    //            System.Data.Entity.SqlServer.SqlProviderServices.Instance);
+    //    }
+    //}
+}
